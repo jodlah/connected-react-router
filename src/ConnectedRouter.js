da17e2f0614c755e6088233a2dbf9c3375d0955f
zaksmok <w.kalazny@defo.pl>
2018-11-12 13:39:40 +0100

    + Fixes for immutablejs

--- src/ConnectedRouter.js
+++ src/ConnectedRouter.js
@@ -1,116 +1,127 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import { Router } from 'react-router'
-import { onLocationChanged } from './actions'
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {Router} from 'react-router';
+import {onLocationChanged} from './actions';
 
 const createConnectedRouter = (structure) => {
-  const { getIn, toJS } = structure
-  /*
-   * ConnectedRouter listens to a history object passed from props.
-   * When history is changed, it dispatches action to redux store.
-   * Then, store will pass props to component to render.
-   * This creates uni-directional flow from history->store->router->components.
-   */
+	const {getIn, toJS} = structure;
 
-  class ConnectedRouter extends Component {
-    constructor(props, context) {
-      super(props)
+	/*
+	 * ConnectedRouter listens to a history object passed from props.
+	 * When history is changed, it dispatches action to redux store.
+	 * Then, store will pass props to component to render.
+	 * This creates uni-directional flow from history->store->router->components.
+	 */
 
-      this.inTimeTravelling = false
+	class ConnectedRouter extends Component {
+		constructor(props, context) {
+			super(props);
 
-      // Subscribe to store changes
-      this.unsubscribe = context.store.subscribe(() => {
-        // Extract store's location
-        const {
-          pathname: pathnameInStore,
-          search: searchInStore,
-          hash: hashInStore,
-        } = toJS(getIn(context.store.getState(), ['router', 'location']))
-        // Extract history's location
-        const {
-          pathname: pathnameInHistory,
-          search: searchInHistory,
-          hash: hashInHistory,
-        } = props.history.location
+			this.inTimeTravelling = false;
 
-        // If we do time travelling, the location in store is changed but location in history is not changed
-        if (pathnameInHistory !== pathnameInStore || searchInHistory !== searchInStore || hashInHistory !== hashInStore) {
-          this.inTimeTravelling = true
-          // Update history's location to match store's location
-          props.history.push({
-            pathname: pathnameInStore,
-            search: searchInStore,
-            hash: hashInStore,
-          })
-        }
-      })
+			// Subscribe to store changes
+			this.unsubscribe = context.store.subscribe(() => {
+				// Extract store's location
+				const {
+					pathname: pathnameInStore,
+					search: searchInStore,
+					hash: hashInStore,
+				} = context.store.getState().toJS().router.location;
+				// Extract history's location
+				const {
+					pathname: pathnameInHistory,
+					search: searchInHistory,
+					hash: hashInHistory,
+				} = props.history.location;
 
-      const handleLocationChange = (location, action) => {
-        // Dispatch onLocationChanged except when we're in time travelling
-        if (!this.inTimeTravelling) {
-          props.onLocationChanged(location, action)
-        } else {
-          this.inTimeTravelling = false
-        }
-      }
+				// If we do time travelling, the location in store is changed but location in history is not changed
+				if (pathnameInHistory !== pathnameInStore || searchInHistory !==
+					searchInStore || hashInHistory !== hashInStore) {
+					this.inTimeTravelling = true;
+					// Update history's location to match store's location
+					props.history.push({
+						pathname: pathnameInStore,
+						search: searchInStore,
+						hash: hashInStore,
+					});
+				}
+			});
 
-      // Listen to history changes
-      this.unlisten = props.history.listen(handleLocationChange)
-      // Dispatch a location change action for the initial location
-      handleLocationChange(props.history.location, props.history.action)
-    }
+			const handleLocationChange = (location, action) => {
+				// Dispatch onLocationChanged except when we're in time travelling
+				if (!this.inTimeTravelling) {
+					props.onLocationChanged(location, action);
+				}
+				else {
+					this.inTimeTravelling = false;
+				}
+			};
 
-    componentWillUnmount() {
-      this.unlisten()
-      this.unsubscribe()
-    }
+			// Listen to history changes
+			this.unlisten = props.history.listen(handleLocationChange);
+			// Dispatch a location change action for the initial location
+			handleLocationChange(props.history.location, props.history.action);
+		}
 
-    render() {
-      const { history, children } = this.props
+		componentWillUnmount() {
+			this.unlisten();
+			this.unsubscribe();
+		}
 
-      return (
-        <Router history={history}>
-          { children }
-        </Router>
-      )
-    }
-  }
+		render() {
+			const {history, children} = this.props;
 
-  ConnectedRouter.contextTypes = {
-    store: PropTypes.shape({
-      getState: PropTypes.func.isRequired,
-      subscribe: PropTypes.func.isRequired,
-    }).isRequired,
-  }
+			return (
+				<Router history={history}>
+					{children}
+				</Router>
+			);
+		}
+	}
 
-  ConnectedRouter.propTypes = {
-    history: PropTypes.shape({
-      action: PropTypes.string.isRequired,
-      listen: PropTypes.func.isRequired,
-      location: PropTypes.object.isRequired,
-      push: PropTypes.func.isRequired,
-    }).isRequired,
-    location: PropTypes.oneOfType([
-      PropTypes.object,
-      PropTypes.string,
-    ]).isRequired,
-    action: PropTypes.string.isRequired,
-    basename: PropTypes.string,
-    children: PropTypes.oneOfType([ PropTypes.func, PropTypes.node ]),
-    onLocationChanged: PropTypes.func.isRequired,
-  }
+	ConnectedRouter.contextTypes = {
+		store: PropTypes.shape({
+			getState: PropTypes.func.isRequired,
+			subscribe: PropTypes.func.isRequired,
+		}).isRequired,
+	};
 
-  const mapStateToProps = state => ({
-    action: getIn(state, ['router', 'action']),
-    location: getIn(state, ['router', 'location']),
-  })
+	ConnectedRouter.propTypes = {
+		history: PropTypes.shape({
+			action: PropTypes.oneOfType([
+				PropTypes.object,
+				PropTypes.string,
+			]).isRequired,
+			listen: PropTypes.func.isRequired,
+			location: PropTypes.object.isRequired,
+			push: PropTypes.func.isRequired,
+		}).isRequired,
+		location: PropTypes.oneOfType([
+			PropTypes.object,
+			PropTypes.string,
+		]).isRequired,
+		action: PropTypes.oneOfType([
+			PropTypes.object,
+			PropTypes.string,
+		]).isRequired,
+		basename: PropTypes.string,
+		children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]),
+		onLocationChanged: PropTypes.func.isRequired,
+	};
 
-  const mapDispatchToProps = dispatch => ({
-    onLocationChanged: (location, action) => dispatch(onLocationChanged(location, action))
-  })
+	const mapStateToProps = state => ({
+		action: state.toJS().router.location,
+		location: state.toJS().router.location,
+	});
 
-  return connect(mapStateToProps, mapDispatchToProps)(ConnectedRouter)
-}
+	const mapDispatchToProps = dispatch => ({
+		onLocationChanged: (location, action) => dispatch(
+			onLocationChanged(location, action),
+		),
+	});
 
-export default createConnectedRouter
+	return connect(mapStateToProps, mapDispatchToProps)(ConnectedRouter);
+};
+
+export default createConnectedRouter;
